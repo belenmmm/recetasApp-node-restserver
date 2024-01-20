681c5178739914b1b66974d0ebf60159de6a9bc5
Monllau <maria.belen.monllau@accenture.com>
2024-01-20 14:31:05 -0300

    fix errros code

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -34,9 +34,9 @@ const usuariosPost = async (req, res = response) => {
 
     if( usuarioDB) {
         return res.status(400).json({
-            errors{
-                msg: `El email ${ usuarioDB.email }, ya está registrado`
-            }
+            errors: [
+                { msg: `El email ${usuarioDB.email}, ya está registrado` }
+              ]
          
         });
       }
